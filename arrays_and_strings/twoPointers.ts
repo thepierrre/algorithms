be3a6becb9e61fefa8d299d5afeeb57fb80c0d2d
thepierrre <piotr.owczarczyk98@gmail.com>
2024-08-31 15:40:33 +0200

    add more functions using two-pointer approach

--- arrays_and_strings/twoPointers.ts
+++ arrays_and_strings/twoPointers.ts
@@ -5,7 +5,7 @@ const word4 = "aeroplane";
 const word5 = "butterfly";
 const word6 = "pistachio";
 
-const checkIfPalindrome = (word: string) => {
+const checkIfPalindrome = (word: string): boolean => {
   let left = 0;
   let right = word.length - 1;
 
@@ -34,7 +34,7 @@ const target2 = 23;
 const nums3 = [2, 4, 6, 8, 10, 12];
 const target3 = 19;
 
-const checkIfPairsEqualsTarget = (nums: number[], target: number) => {
+const checkIfPairsEqualsTarget = (nums: number[], target: number): boolean => {
   let left = 0;
   let right = nums.length - 1;
 
@@ -64,13 +64,91 @@ console.log(checkIfPairsEqualsTarget(nums3, target3)); // false
 
 const arr1 = [1, 4, 7, 20];
 const arr2 = [3, 5, 6];
+const arr3 = [1, 2, 8];
+const arr4 = [3, 4, 6, 10];
 
-const combineArraysAndSort = (arr1: number[], arr2: number[]) => {
+// time complexity = O(n)
+const combineArrays = (arr1: number[], arr2: number[]): number[] => {
   let i = 0,
     j = 0;
-  let output = [];
+  let output: number[] = [];
 
-  while (i < arr1.length) {}
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      output.push(arr1[i]);
+      i++;
+    } else {
+      output.push(arr2[j]);
+      j++;
+    }
+  }
 
-  while (j < arr2.length) {}
+  while (i < arr1.length) {
+    output.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    output.push(arr2[j]);
+    j++;
+  }
+
+  return output;
+};
+
+console.log(combineArrays(arr3, arr4));
+
+// Given two strings s and t,
+// return true if s is a subsequence of t, or false otherwise.
+// (The order of the characters counts, but they can be separated by other characters;
+// e.g. "ace" is a substring of "abcde" but "aec" isn't).
+
+const string1 = "ace";
+const string2 = "aec";
+const string3 = "abcde";
+
+const checkIfSubsequence = (s: string, t: string): boolean => {
+  let output = "";
+  let i = 0,
+    j = 0;
+
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) {
+      output += s[i];
+      i++;
+    }
+    j++;
+  }
+
+  return s === output;
+};
+
+console.log(checkIfSubsequence(string1, string3));
+
+// Reverse a string
+
+const reverseString = (s: string[]): string[] => {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    [s[left], s[right]] = [s[right], s[left]];
+    left++;
+    right--;
+  }
+
+  return s;
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
+
+// Given an integer array nums sorted in non-decreasing order,
+// return an array of the squares of each number sorted in non-decreasing order.
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+
+let nums = [-4, -1, 0, 3, 10];
+
+const returnSquaresNonDecreasing = (arr: number[]): number[] => {
+  let output: number = [];
 };
