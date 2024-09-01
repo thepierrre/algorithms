bf67f2af1b44b6116a67d7380469b45226e26f83
thepierrre <piotr.owczarczyk98@gmail.com>
2024-09-01 18:58:58 +0200

    add more slidingWindow functions + add prefixSum.js

---
+++ arrays_and_strings/prefixSum.js
@@ -0,0 +1,6 @@
+// Given an integer array nums, an array queries where queries[i] = [x, y] and an integer limit, 
+// return a boolean array that represents the answer to each query. 
+// A query is true if the sum of the subarray from x to y is less than limit, or false otherwise.
+
+// For example, given nums = [1, 6, 3, 2, 7, 2], queries = [[0, 3], [2, 5], [2, 4]], and limit = 13, 
+// the answer is [true, false, true]. For each query, the subarray sums are [12, 14, 12].
--- arrays_and_strings/slidingWindow.js
+++ arrays_and_strings/slidingWindow.js
@@ -138,3 +138,30 @@ console.log(findSubarrayWithMaxAverage([1, 12, -5, -6, 50, 3], 4))
 
 // Given a binary array nums and an integer k, 
 // return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+const findMaxConsecutiveOnes = (nums, k) => {
+    let left = 0;
+    let counterOfZeros = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) {
+            counterOfZeros++;
+        }
+
+        while (counterOfZeros > k) {
+            if (nums[left] === 0) {
+                counterOfZeros--;
+            }
+            left++;
+        }
+
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+
+}
+
+console.log(findMaxConsecutiveOnes([1,1,1,0,0,0,1,1,1,1,0], 2)) // 6
+console.log(findMaxConsecutiveOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3)) // 10
